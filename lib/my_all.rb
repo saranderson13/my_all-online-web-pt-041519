459fa2d56792a30975991b935ccb3e849429b792
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 01:53:38 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,8 @@ def my_all?(collection)
   i = 0
   
   while i < collection.length
-    break if yield(collection[i]) == false
+    return false if yield(collection[i]) == false
     i += 1
   end
-  
+  true
 end
