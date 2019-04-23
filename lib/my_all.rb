403a48cc1e688434313e215589ebbe06bb1d070c
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 01:56:48 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,10 +2,22 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  
+  block_return_values = []
   while i < collection.length
-    return false if yield(collection[i]) == false
+    block_return_values << yield(collection[i])
     i += 1
   end
-  true
+  block_return_values.include?(false) ? false : true
 end
+
+
+# ALTERNATE VERSION
+# def my_all?(collection)
+#   i = 0
+  
+#   while i < collection.length
+#     return false if yield(collection[i]) == false
+#     i += 1
+#   end
+#   true
+# end
